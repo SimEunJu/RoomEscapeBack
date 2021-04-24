dee9451ef2a11a840944df543f1f9c078bdab227
SimEunJu <wndmstla123@gmail.com>
2021-04-24 21:18:39 +0900

    feat: controller requestParam enum 소문자 매핑 설정

---
+++ src/main/java/com/sej/escape/constants/FileType.java
@@ -0,0 +1,5 @@
+package com.sej.escape.constants;
+
+public enum FileType {
+    BOARD, STORE, THEME
+}
---
+++ src/main/java/com/sej/escape/service/file/BoardFileService.java
@@ -0,0 +1,10 @@
+package com.sej.escape.service.file;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardFileService {
+
+}
