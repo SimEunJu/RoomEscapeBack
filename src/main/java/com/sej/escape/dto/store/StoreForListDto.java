1ac0f235aa27a5a8e92764dd367bfd7c61f29ffc
SimEunJu <wndmstla123@gmail.com>
2021-04-28 01:31:47 +0900

    feat: 가게 리스트용 dto 생성

---
+++ src/main/java/com/sej/escape/dto/store/StoreForListDto.java
@@ -0,0 +1,14 @@
+package com.sej.escape.dto.store;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StoreForListDto {
+
+    protected long id;
+    protected String name;
+}
